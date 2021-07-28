35176a950c6cdab86ea58bb7a4f750d0738bf2bb
Mischa <35232766+mischaguilty@users.noreply.github.com>
2021-07-28 14:53:19 +0300

    Update VpicnhtsaServiceProvider.php

--- src/VpicnhtsaServiceProvider.php
+++ src/VpicnhtsaServiceProvider.php
@@ -14,41 +14,7 @@ class VpicnhtsaServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'vpicnhtsa');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-
-        Actions::registerRoutesForAction(SearchVin::class);
-        Actions::registerRoutesForAction(SearchNumber::class);
-
-//        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-
-//        $this->loadViewsFrom(__DIR__.'/../resources/views', 'vpicnhtsa');
-
         if ($this->app->runningInConsole()) {
-//            $this->publishes([
-//                __DIR__.'/../config/config.php' => config_path('vpicnhtsa.php'),
-//            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/vpicnhtsa'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/vpicnhtsa'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/vpicnhtsa'),
-            ], 'lang');*/
-
-            // Registering package commands.
             $this->commands([
                 SearchVin::class,
                 SearchNumber::class,
@@ -61,9 +27,6 @@ class VpicnhtsaServiceProvider extends ServiceProvider
      */
     public function register()
     {
-//        // Automatically apply the package configuration
-//        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'vpicnhtsa');
-
         // Register the main class to use with the facade
         $this->app->singleton('vpicnhtsa', function () {
             return new Vpicnhtsa;
